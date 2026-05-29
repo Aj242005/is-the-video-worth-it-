651020f211e211893c8836562685dbd124a57f42
Aj242005 <akshitjain6661@gmail.com>
2026-05-29 16:22:02 +0530

    finished the retriver

--- controllers.py
+++ controllers.py
@@ -9,9 +9,10 @@ from youtube_transcript_api._errors import TranscriptsDisabled, NoTranscriptFoun
 from langchain_classic.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma.vectorstores import Chroma
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_core.documents import Document
 from pydantic import SecretStr
 import os
-counter = 1
+import uuid
 gemini_api = SecretStr(os.getenv("GOOGLE_API_KEY",""))
 
 tenant = os.getenv("tenant","")
@@ -32,7 +33,7 @@ def _transcript(video_id: str) -> str:
         return "No transcript found."
 
 
-def handle_url_and_prompt(yt_object: Yturl) -> List[str]:
+def handle_url_and_prompt(yt_object: Yturl) -> List[Document]:
     '''Main entry point controller.'''
     url = yt_object.url.strip()
     opts: dict[str, Any] = {"skip_download": True, "quiet": True, "extract_flat": True}
@@ -51,13 +52,13 @@ def handle_url_and_prompt(yt_object: Yturl) -> List[str]:
 
     vector_store = Chroma(
         chroma_cloud_api_key = chroma_api_key,
-        collection_name = "default-collection",
+        collection_name = f"default-collection {uuid.uuid4()}",
         create_collection_if_not_exists=True,
         database = database,
         tenant = tenant,
-        embedding_function = GoogleGenerativeAIEmbeddings( 
+        embedding_function = GoogleGenerativeAIEmbeddings(
             api_key = gemini_api,
-            model = "models/gemini-embedding-001" 
+            model = "models/gemini-embedding-001"
         )
     )
     response = whole_workflow(
@@ -66,10 +67,10 @@ def handle_url_and_prompt(yt_object: Yturl) -> List[str]:
         vector_store = vector_store
     )
     return response
-def whole_workflow( prompt : str, transcripts: str, vector_store : Chroma) -> List[str]:
+def whole_workflow( prompt : str, transcripts: str, vector_store : Chroma) -> List[Document]:
     '''This is the whole workflowwwww '''
     if (not transcripts) or (not prompt) :
-        raise ValueError("We require transcripts and prompt to be non empty")   
+        raise ValueError("We require transcripts and prompt to be non empty")
     if transcripts and prompt: #ik this is uncessary but coool
         return _workflow(transcripts=transcripts,prompt=prompt,vector_store=vector_store)
     else:
@@ -77,8 +78,12 @@ def whole_workflow( prompt : str, transcripts: str, vector_store : Chroma) -> Li
 
 
 
-def _workflow( transcripts : str, vector_store : Chroma, prompt : str = "" ) -> List[str]:
+def _workflow( transcripts : str, vector_store : Chroma, prompt : str = "" ) -> List[Document]:
     docs = recursive_text_splitter.create_documents([transcripts])
     list_of_ids = vector_store.add_documents(docs)
+
+    retriver = vector_store.as_retriever(k = 5, search_type = "mmr")
+    documents = retriver.invoke(input=prompt)
     print(f" prompt : {prompt}")
-    return list_of_ids
+    print(list_of_ids)
+    return documents
